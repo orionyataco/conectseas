e1837c24415e65848319cdb6d864b8cb83edd2da
orionyataco <orionyataco@gmail.com>
2026-03-09 16:45:27 -0300

    feat: configurar knex para migrations e atualizar docker-compose

---
+++ knexfile.js
@@ -0,0 +1,45 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+export default {
+  development: {
+    client: 'postgresql',
+    connection: {
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432'),
+      user: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || '',
+      database: process.env.DB_NAME || 'conectseas',
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './server/migrations'
+    }
+  },
+  production: {
+    client: 'postgresql',
+    connection: {
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432'),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './server/migrations'
+    }
+  }
+};
